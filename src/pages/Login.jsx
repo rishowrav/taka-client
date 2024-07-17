f56f6d17f7e0d0574e0df398def32b3e46eafad1
rishowrav <rishowrav@gmail.com>
2024-07-17 08:37:43 +0600

    added: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Taka from "/assets/images/taka.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex h-screen w-full items-center bg-[url(https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80)] justify-center bg-gray-900 bg-cover bg-no-repeat">
+      <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+        <div className="text-white">
+          <div className="mb-8 flex flex-col items-center">
+            <img src={Taka} width="150" alt="" srcset="" />
+            <h1 className="mb-2 text-2xl">Taka.com</h1>
+            <span className="text-gray-300">
+              Don't have an account yet?{" "}
+              <Link
+                className="text-[#F59108] font-bold underline"
+                to="/registration"
+              >
+                Register
+              </Link>
+            </span>
+          </div>
+          <form action="#">
+            <div className="mb-4 text-lg">
+              <input
+                className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                type="text"
+                name="name"
+                placeholder="Email or Phone Number"
+              />
+            </div>
+
+            <div className="mb-4 text-lg">
+              <input
+                className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                type="Password"
+                name="name"
+                placeholder="PIN"
+              />
+            </div>
+            <div className="mt-8 flex justify-center text-lg text-black">
+              <button
+                type="submit"
+                className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
     element: <Login />,
   },
   {
-    path: "/registeration",
+    path: "/registration",
     element: <Registration />,
   },
   {
